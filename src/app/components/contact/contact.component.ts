0e4e79eff5f0467d63436cdaca5bc217c8ea24d4
wesley mbate <wmbate@meliora.tech>
2023-09-06 20:56:39 +0300

    refined content across the app

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { style, state, animate, transition, trigger, query, stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [   // :enter is alias to 'void => *'
+        style({ opacity: 0 }),
+        animate(600, style({ opacity: 1 }))
+      ])
+    ]),
+    trigger('fadeInGrow', [
+      transition(':enter', [
+        query(':enter', [
+          style({ opacity: 0, marginTop: 40 }),
+          stagger('120ms', [
+            animate('500ms ease', style({ opacity: 1, marginTop: 0 }))
+          ])
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class ContactComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+    setTimeout(() => {
+      window.scroll(0, 0);
+    }, 10);
   }
 
 }
